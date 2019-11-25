36050d11c8e514bf402b8fdbf663069e7c92738c
besroka <bartosz.sroka4@gmail.com>
2019-11-25 10:52:51 +0100

    Updates

---
+++ precache-manifest.279bc967d78bfb96c058c0a022797a8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40e75a32749f80e2c08b90dcd99ca4ed",
+    "url": "/ToDoApp-react/index.html"
+  },
+  {
+    "revision": "f6d6a2686659ab2464ec",
+    "url": "/ToDoApp-react/static/css/main.ea15a25d.chunk.css"
+  },
+  {
+    "revision": "562776ea9e21d5c56b8d",
+    "url": "/ToDoApp-react/static/js/2.25a43257.chunk.js"
+  },
+  {
+    "revision": "f6d6a2686659ab2464ec",
+    "url": "/ToDoApp-react/static/js/main.172f31f4.chunk.js"
+  },
+  {
+    "revision": "b894fe10c9ee7db2b42c",
+    "url": "/ToDoApp-react/static/js/runtime-main.f8e85244.js"
+  },
+  {
+    "revision": "9568c0c203c674daca58bcf91785f097",
+    "url": "/ToDoApp-react/static/media/checklist.9568c0c2.png"
+  }
+]);
